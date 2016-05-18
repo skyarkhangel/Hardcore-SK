7f34f052de0e0d356b99a3a0c032bb54850fdec7
skyarkhangel <skyarkhangel@gmail.com>
2016-05-19 01:23:07 +0300

    Pawn State Icons, PrepareCarefully, Skynet

---
+++ Mods/Pawn State Icons/Source/PSI/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("PSI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PSI")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f0c2500f-aad0-4087-b7aa-f5c0d07f00ab")]
+[assembly: AssemblyVersion("0.13.1.0")]
+[assembly: AssemblyFileVersion("0.13.1.0")]
