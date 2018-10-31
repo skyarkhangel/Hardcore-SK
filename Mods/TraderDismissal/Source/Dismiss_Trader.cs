93a3b4645e9f4611553df88dac1a98360a391c1b
Szara/ManOfHonor <tenmanikaosu@gmail.com>
2018-10-31 15:27:30 -0400

    Re-add ZiTools, add TraderDimiss, Snapout
    Verified all are working without changes.

---
+++ Mods/TraderDismissal/Source/Dismiss_Trader.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using Verse;
+using Harmony;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse.AI;
+
+namespace Dismiss_Trader
+{
+    [StaticConstructorOnStartup]
+    public class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("mehni.rimworld.traderdismissal.main");
+
+            harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap), "AddHumanlikeOrders"), null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(FloatMenuMakerMap_AddHumanlikeOrdersToDismissTraders_PostFix)));
+        }
+
+        private static void FloatMenuMakerMap_AddHumanlikeOrdersToDismissTraders_PostFix(ref Vector3 clickPos, ref Pawn pawn, ref List<FloatMenuOption> opts)
+        {
+            foreach (LocalTargetInfo target in GenUI.TargetsAt(clickPos, TargetingParameters.ForTrade(), true))
+            {
+                Pawn localpawn = pawn;
+                LocalTargetInfo dest = target;
+                if (!pawn.CanReach(dest, PathEndMode.OnCell, Danger.Deadly)) return;
+                if (pawn.skills.GetSkill(SkillDefOf.Social).TotallyDisabled) return;
+                
+                Pawn pTarg = (Pawn)dest.Thing;
+                void Action()
+                {
+                    Job job = new Job(TraderDismissalJobDefs.DismissTrader, pTarg) { playerForced = true };
+                    localpawn.jobs.TryTakeOrderedJob(job);
+                }
+
+                string str = string.Empty;
+                if (pTarg.Faction != null)
+                {
+                    str = " (" + pTarg.Faction.Name + ")";
+                }
+
+                string label = "GETOUT".Translate(pTarg.LabelShort + ", " + pTarg.TraderKind.label) + str;
+
+                opts.Add(FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption(label, Action, MenuOptionPriority.InitiateSocial, null, dest.Thing), pawn, pTarg));
+            }
+        }
+    }
+
+    [DefOf]
+    public class TraderDismissalJobDefs
+    {
+        public static JobDef DismissTrader;
+    }
+}
---
+++ Mods/TraderDismissal/Source/JobDriver_DismissTrader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+using Verse.AI.Group;
+
+namespace Dismiss_Trader
+{
+    class JobDriver_DismissTrader : JobDriver
+    {
+        private Pawn Trader => (Pawn)base.TargetThingA;
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed) => this.pawn.Reserve(this.Trader, this.job);
+
+        //approach: find Lord transition that is the regular time-out and add another (very short) Trigger_TicksPassed. That'll then fire, and the traders will leave.
+
+        //other (failed) approaches:
+        //- inheriting from LordJob_TradeWithColony and overriding the stategraph. Set a bool in the job, which works as a trigger. Still seems like the "correct" and OOP approach, but I suck at C#
+        //- adding new LordToil_ExitMapAndEscortCarriers() & telling the lord to jump to it. (lord null, somehow not registered in graph?)
+        //- Outright removing the lord. Works, but also removes the traderflag, defending at exit and the group behaviour. Bad.
+        //- transpile all the things! (ain't noone got time for that)
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDespawnedOrNull(TargetIndex.A);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch).FailOn(() => !this.Trader.CanTradeNow);
+            Toil trade = new Toil();
+            trade.initAction = () =>
+            {
+                if (this.Trader.CanTradeNow)
+                {
+                    Lord lord = Trader.GetLord();
+                    List<Transition> transitions = lord.Graph.transitions.ToList();
+                    foreach (Transition transition in transitions)
+                    {
+                        foreach (Trigger trigger in transition.triggers)
+                        {
+                            if (trigger is Trigger_TicksPassed && !transition.preActions.Any(x => x is TransitionAction_CheckGiveGift))
+                            {
+                                transition.triggers.Add(new Trigger_TicksPassed(20));
+                                break;
+                            }
+                        }
+                    }
+                }
+            };
+            yield return trade;
+        }
+    }
+}
