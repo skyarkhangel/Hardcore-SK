c0fa2d2f4c20896576b7092e73504cfcfc8f88cc
skyarkhangel <skyarkhangel@gmail.com>
2019-03-20 18:23:51 +0300

    Added LoadUp mod, should slightly increase game load. Should be fixed broken poison arrows.

---
+++ Mods/RIMMSLoadUp/Source/RIMMSLoadUp/LoadUp.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Malte Schulze
+ * Date: 18.03.2019
+ * Time: 11:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Reflection;
+using Harmony;
+using Verse;
+
+namespace RIMMSLoadUp
+{
+	/// <summary>
+	/// Description of LoadUp.
+	/// </summary>
+	public class LoadUp : Mod
+	{
+		public LoadUp(ModContentPack content) : base(content) {
+			HarmonyInstance harmony = HarmonyInstance.Create("RIMMSLoadUp");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
