892aa3b23ae9f0bdb167e26bc89135734f59a176
skyarkhangel <skyarkhangel@gmail.com>
2020-11-01 14:08:35 +0300

    Added HiveAttackTargetDisabler mod by PirateBY. Disable auto-attack Hive's by non drafted and drafted colonists.

---
+++ Mods/HiveAttackTargetDisabler/Src/HiveAttackTargetDisabler.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using Verse.AI;
+
+
+namespace HiveAttackTargetDisabler
+{
+    [HarmonyPatch(typeof(Hive), nameof(Hive.ThreatDisabled))]
+    [StaticConstructorOnStartup]
+    public class Patch
+    {
+        static Patch() => new Harmony("HiveAttackTargetDisabler").PatchAll();
+
+        [HarmonyPrefix]
+        public static bool ThreatDisabled(IAttackTargetSearcher disabledFor, ref bool __result) => !(__result = true);
+    }
+}
