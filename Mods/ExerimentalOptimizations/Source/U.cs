09a502d5861e4cee2c11a6df10d59db853ee72f3
bananasss00 <bananasss00@yandex.ru>
2020-11-23 12:06:27 +0300

    Mod ExperimentalOptimizations https://discord.com/channels/272340793174392832/745746107329413121/779025443339763743

---
+++ Mods/ExerimentalOptimizations/Source/U.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace ExperimentalOptimizations
+{
+    public class U
+    {
+        public static IEnumerable<object> DefDatabaseAllDefs(string className)
+        {
+            Type genericType = typeof(DefDatabase<>).MakeGenericType( AccessTools.TypeByName(className) );
+            IEnumerable collection = (IEnumerable)genericType.InvokeMember("get_AllDefsListForReading", BindingFlags.InvokeMethod, null, null, new object[0]);
+
+            var enumerator = collection.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+}
