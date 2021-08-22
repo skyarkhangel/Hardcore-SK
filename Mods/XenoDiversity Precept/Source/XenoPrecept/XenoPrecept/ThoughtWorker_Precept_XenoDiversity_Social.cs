e368f3ec73dfdfb0a348d100686d5b9dc786fc55
ArpoMo6 <62516232+ArpoMo6@users.noreply.github.com>
2021-08-22 14:34:22 +0500

    Added XenoDiversity Precept mod with rus translation

---
+++ Mods/XenoDiversity Precept/Source/XenoPrecept/XenoPrecept/ThoughtWorker_Precept_XenoDiversity_Social.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace XenoPrecept
+{
+	public class ThoughtWorker_Precept_XenoDiversity_Social : ThoughtWorker_Precept_Social
+	{
+		protected override ThoughtState ShouldHaveThought(Pawn p, Pawn otherPawn)
+		{
+			return p.Faction == otherPawn.Faction && p.def.label != otherPawn.def.label ;
+		}
+	}
+}
