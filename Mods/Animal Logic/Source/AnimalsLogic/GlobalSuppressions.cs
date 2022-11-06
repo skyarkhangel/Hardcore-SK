847d368f4a03eb4d1a295006c6a7a250bdaa30da
TheLoneTec <kaceysherrard@hotmail.com>
2022-11-06 01:43:50 +0000

    Cleanup...again

--- Mods/Animal Logic/Source/AnimalsLogic/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Harmony uses them.")]
