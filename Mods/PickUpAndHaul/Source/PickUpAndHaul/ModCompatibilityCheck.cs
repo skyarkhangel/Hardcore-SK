76c3753d5c3cb275139ab3021eada4603b441c32
TheLoneTec <kaceysherrard@hotmail.com>
2022-12-08 23:58:21 +0000

    Add Pick Up And Haul

---
+++ Mods/PickUpAndHaul/Source/PickUpAndHaul/ModCompatibilityCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace PickUpAndHaul;
+public static class ModCompatibilityCheck
+{
+	public static bool CombatExtendedIsActive { get; } = ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Combat Extended");
+
+	public static bool AllowToolIsActive { get; } = ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Allow Tool");
+
+	public static bool ExtendedStorageIsActive { get; } = ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "ExtendedStorageFluffyHarmonised")
+		|| ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Extended Storage")
+		|| ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Core SK");
+
+	public static bool HCSKIsActive { get; } = ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Core SK");
+}
---
+++ Mods/PickUpAndHaul/Source/PickUpAndHaul/PawnUnloadChecker.cs
@@ -0,0 +1,60 @@
+﻿namespace PickUpAndHaul;
+public class PawnUnloadChecker
+{
+	public static void CheckIfPawnShouldUnloadInventory(Pawn pawn, bool forced = false)
+	{
+		var job = JobMaker.MakeJob(PickUpAndHaulJobDefOf.UnloadYourHauledInventory, pawn);
+		var itemsTakenToInventory = pawn?.GetComp<CompHauledToInventory>();
+
+		if (itemsTakenToInventory == null)
+        {
+            return;
+        }
+
+        var carriedThing = itemsTakenToInventory.GetHashSet();
+
+		if (pawn.Faction != Faction.OfPlayerSilentFail || !Settings.IsAllowedRace(pawn.RaceProps)
+			|| carriedThing == null || carriedThing.Count == 0
+			|| pawn.inventory.innerContainer is not { } inventoryContainer || inventoryContainer.Count == 0)
+		{
+			return;
+		}
+
+		if ((forced && job.TryMakePreToilReservations(pawn, false))
+			|| ((MassUtility.EncumbrancePercent(pawn) >= 0.90f || carriedThing.Count >= 1)
+			&& job.TryMakePreToilReservations(pawn, false)))
+		{
+			pawn.jobs.jobQueue.EnqueueFirst(job, JobTag.Misc);
+			return;
+		}
+
+		if (inventoryContainer.Count >= 1)
+		{
+			for (var i = 0; i < inventoryContainer.Count; i++)
+			{
+				var compRottable = inventoryContainer[i].TryGetComp<CompRottable>();
+
+				if (compRottable?.TicksUntilRotAtCurrentTemp < 30000)
+				{
+					pawn.jobs.jobQueue.EnqueueFirst(job, JobTag.Misc);
+					return;
+				}
+			}
+		}
+
+		if (Find.TickManager.TicksGame % 50 == 0 && inventoryContainer.Count < carriedThing.Count)
+		{
+			Verse.Log.Warning("[PickUpAndHaul] " + pawn + " inventory was found out of sync with haul index. Pawn will drop their inventory.");
+			carriedThing.Clear();
+			pawn.inventory.UnloadEverything = true;
+		}
+	}
+}
+
+[DefOf]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Has to match defName")]
+public static class PickUpAndHaulJobDefOf
+{
+	public static JobDef UnloadYourHauledInventory;
+	public static JobDef HaulToInventory;
+}
