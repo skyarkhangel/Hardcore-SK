666a8d2ec53d43e6de1199d73b19b57a2437e465
TheLoneTec <kaceysherrard@hotmail.com>
2022-12-09 16:51:40 +0000

    Pick Up And Haul + Allow Tools fix + other fixes
    Toggleable Overlays
    Adjusted Campfire smoke (SmokeFX)
    Core_SK Apparel_NightVision wornTexture updated
    Core_Sk Biotech Patches Buildings_Furniture.xml crib corrected
    Pick Up And Haul Added + Updated
    Allow Tools Updated

---
+++ Mods/PickUpAndHaul/Source/PickUpAndHaul/CompHauledToInventory.cs
@@ -0,0 +1,20 @@
+﻿namespace PickUpAndHaul;
+
+public class CompHauledToInventory : ThingComp
+{
+	private HashSet<Thing> takenToInventory = new();
+
+	public HashSet<Thing> GetHashSet()
+	{
+		takenToInventory.RemoveWhere(x => x == null);
+		return takenToInventory;
+	}
+
+	public void RegisterHauledItem(Thing thing) => takenToInventory.Add(thing);
+
+	public override void PostExposeData()
+	{
+		base.PostExposeData();
+		Scribe_Collections.Look(ref takenToInventory, "ThingsHauledToInventory", LookMode.Reference);
+	}
+}
---
+++ Mods/PickUpAndHaul/Source/PickUpAndHaulv1.0/IHoldMultipleThings_Support.cs
@@ -0,0 +1,54 @@
+﻿using Verse;
+
+namespace PickUpAndHaul
+{
+    using System.Linq;
+
+    public class HoldMultipleThings_Support
+    {
+        // ReSharper disable SuspiciousTypeConversion.Global
+        public static bool CapacityAt(Thing thing, IntVec3 storeCell, Map map, out int capacity)
+        {
+            capacity = 0;
+
+            var compOfHolding = (map.haulDestinationManager.SlotGroupParentAt(storeCell) as ThingWithComps)?
+               .AllComps.FirstOrDefault(x => x is IHoldMultipleThings.IHoldMultipleThings);
+
+            if (compOfHolding is IHoldMultipleThings.IHoldMultipleThings holderOfThings)
+            {
+                return holderOfThings.CapacityAt(thing, storeCell, map, out capacity);
+            }
+
+            foreach (Thing t in storeCell.GetThingList(map))
+            {
+                if (t is IHoldMultipleThings.IHoldMultipleThings holderOfMultipleThings)
+                {
+                    return holderOfMultipleThings.CapacityAt(thing, storeCell, map, out capacity);
+                }
+            }
+
+            return false;
+        }
+
+        public static bool StackableAt(Thing thing, IntVec3 storeCell, Map map)
+        {
+            var compOfHolding = (map.haulDestinationManager.SlotGroupParentAt(storeCell) as ThingWithComps)?
+               .AllComps.FirstOrDefault(x => x is IHoldMultipleThings.IHoldMultipleThings);
+
+            if (compOfHolding is IHoldMultipleThings.IHoldMultipleThings holderOfThings)
+            {
+                return holderOfThings.StackableAt(thing, storeCell, map);
+            }
+
+            foreach (Thing t in storeCell.GetThingList(map))
+            {
+                if (t is IHoldMultipleThings.IHoldMultipleThings holderOfMultipleThings)
+                {
+                    return holderOfMultipleThings.StackableAt(thing, storeCell, map);
+                }
+            }
+
+            return false;
+        }
+    }
+}
