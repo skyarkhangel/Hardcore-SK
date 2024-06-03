aff8b0bc0b87b85ea523292e4c13123ee108a4dc
Manifold Paradox <masakitenchi@users.noreply.github.com>
2024-06-03 20:09:56 +0900

    Formalize update_gitattribute.py

--- update_gitattribute.py
+++ update_gitattribute.py
@@ -16,22 +16,24 @@ class Mod:
     path: str
     supported_versions: list[str] = field(default_factory=list)
 
-folder_dict: dict[str, Mod] = {}
-
-
-
 
 def get_modsconfig() -> list[str]:
+    """
+    Get the list of active mods from ModsConfig.xml
+    """
     result = []
     with open("./ModsConfig.xml", "r", encoding="utf-8") as f:
         tree = ET.parse(f)
         root = tree.getroot()
-        print(root.tag)
+        #print(root.tag)
         for mod in root.find("activeMods").iter():
             result.append(mod.text.lower())
     return result
 
 def is_hsk(path: str) -> bool:
+    """
+    Check if the mod is a HSK mod by checking if the mod has a file named "HSK" in root folder
+    """
     with os.scandir(path) as folder:
         for entry in folder:
             if entry.is_file() and entry.name == "HSK":
@@ -40,7 +42,11 @@ def is_hsk(path: str) -> bool:
     return False
 
 
-def build_dict():
+def build_dict() -> dict[str, Mod]:
+    """
+    Build a dict of mods with package_id as key and Mod object as value
+    """
+    folder_dict = {}
     for entry in filter(lambda x: x.is_dir(), os.scandir("./Mods")):
         subdir_path = entry.path
         if is_hsk(subdir_path):
@@ -55,21 +61,24 @@ def build_dict():
                                   supported_versions=[x.text for x in root.find("supportedVersions").iter()])
                         folder_dict[mod.package_id] = mod
                 break
+    return folder_dict
 
 
-def output(enabled: list[str]) -> None:
+def output(enabled: list[str], mod_dict: dict[str, Mod]) -> None:
+    """
+    Set export-ignore for mods that are not enabled in ModsConfig.xml and not supported in 1.5
+    """
     with open(".gitattributes", "w", encoding='utf-8') as f:
-        for k, v in folder_dict.items():
-            print(f"{k} {v}")
+        for k, v in mod_dict.items():
+            #print(f"{k} {v}")
             if k not in enabled and '1.5' not in v.supported_versions:
                 f.write(f"\"{v.path.removeprefix('./')}\" export-ignore\n")
 
 
-def main():
+def main() -> None:
     enabled = get_modsconfig()
     print(enabled)
-    build_dict()
-    output(enabled)
+    output(enabled, build_dict())
 
 
 if __name__ == "__main__":
